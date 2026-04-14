af527425c42a595d202e88f7adf226e34f305be8
prabin982 <pdhungana061@gmail.com>
2026-04-14 20:21:14 +0545

    Enable SSL for PostgreSQL cloud deployment

--- server/config/db.js
+++ server/config/db.js
@@ -5,7 +5,13 @@ dotenv.config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
-    logging: false, // Set to console.log to see SQL queries
+    logging: false,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
 });
 
 const connectDB = async () => {
